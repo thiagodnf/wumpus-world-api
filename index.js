228198cbfbad6f0784efa5b026d27d9f391aa7e2
Thiago Ferreira <thiagodnf@users.noreply.github.com>
2025-09-09 10:16:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -19,9 +19,80 @@
 
     "use strict";
 
+    class RandomUtils {
+
+        static int(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+
+        static element(array) {
+            const index = RandomUtils.int(0, array.size - 1);
+            return array[index];
+        }
+    }
+
+    class SetUtils {
+
+        static createFromArray(matrix) {
+            return new Set(matrix.flatMap((row, i) => row.map((_, j) => `${i},${j}`)));
+        }
+
+        static removeValues(set, values) {
+
+            for (const value of values) {
+                set.delete(value);
+            }
+
+            return set;
+        }
+    }
+
+    class ArrayUtils {
+
+        static create(lines, columns, defaultValue = 0) {
+            return Array.from({ length: lines }, () => Array(columns).fill(defaultValue));
+        }
+
+
+
+        // static getIndexes(matrix) {
+        //     return matrix.flatMap((row, i) => row.map((_, j) => [i, j]));
+        // }
+
+        static removeByValues(array, values) {
+
+            let filtered = [];
+
+            array.forEach(el1 => {
+                if (!ArrayUtils.contains(values, el1)) {
+                    filtered.push(el1);
+                }
+            });
+
+            return filtered;
+        }
+    }
+
     function create(lines, columns) {
-        console.log(lines, columns);
-        console.log("Example of UMD module system");
+
+        let matrix = ArrayUtils.create(lines, columns);
+
+        let indexes = SetUtils.createFromArray(matrix);
+
+        indexes = SetUtils.removeValues(indexes, ["0,0", "0,1", "1,0", "1,1"])
+
+        console.log(indexes)
+
+        console.log(indexes.keys())
+
+        console.log(RandomUtils.element(indexes))
+        console.log(RandomUtils.element(indexes))
+        console.log(RandomUtils.element(indexes))
+        console.log(RandomUtils.element(indexes))
+3
+        // let indexes = MatrixUtils.create(lines, columns);
+
+        return [];
     }
 
     // expose log to other modules
